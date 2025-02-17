05dd644eb8b2e67c47a7b261ef52af0af88fe0d0
Dana <beware.14b@gmail.com>
2025-02-17 21:41:16 +0200

    update to new template

---
+++ DisassembleChiseledBlock/Systems/Core.cs
@@ -0,0 +1,13 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace DisassembleChiseledBlock;
+
+public class Core : ModSystem
+{
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+
+        Mod.Logger.Event("started '{0}' mod", Mod.Info.Name);
+    }
+}
